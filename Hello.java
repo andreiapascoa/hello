5b4473c2370c7cd95feb544e650d4f0446c064f7
andreiapascoa <andreiapascoa@gmail.com>
2023-12-17 21:00:51 +0000

    add dear string

--- Hello.java
+++ Hello.java
@@ -2,11 +2,11 @@ class Hello {
 
 	public static void main(String[] args) {
 
-	String hello = "Hello";
+	String hello = "Hello, ";
+	String dear = "Dear ";
+	String world = "World";
 	System.out.println(hello);	
-
-	String world = ", World";
-	System.out.println(hello);	
+	System.out.println(dear);	
 	System.out.println(world);	
 }
 
