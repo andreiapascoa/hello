127f436f6f94f0ce2ba6a1dc4fc259550f8ba3d2
andreiapascoa <andreiapascoa@gmail.com>
2023-12-19 20:05:15 +0000

    deleted sout

--- Hello.java
+++ Hello.java
@@ -2,16 +2,6 @@ class Hello {
 
 	public static void main(String[] args) {
 
-	String hello = "Hello, ";
-	String dear = "Dear ";
-	String world = "World!!";	
-	System.out.println(hello);	
-	System.out.println(dear);	
-	System.out.println(world);
-
-
-	System.out.println("Creating a diff, part 1, disaster is afoot");	
-	System.out.println("Creating a diff, part 2, what will happen?");	
 }
 
 }
